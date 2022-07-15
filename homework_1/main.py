37fab36195689b36bcdcf81d0e9136623269f08d
Eugene-TechnoNinja <technoninja@ukr.net>
2022-07-15 15:10:27 +0300

    code refactoring

--- homework_1/main.py
+++ homework_1/main.py
@@ -10,28 +10,18 @@ keys_3 = ["bar", "foobar"]
 
 
 def filter_values(data, keys):
-    results = []
+    temp, results = [], []
     for current_item in data:
         for current_key in keys:
-            value_item = current_item.get(current_key)
-            if len(results) == 0:
+            current_value = current_item.get(current_key)
+            if current_value not in temp:
+                temp.append(current_value)
                 results.append(current_item)
-            else:
-                for current_result in results:
-                    value_result = current_result.get(current_key)
-                    if value_result != value_item:
-                        results.append(current_item)
-                    break
+            break
     return results
 
 
 if __name__ == "__main__":
     print(filter_values(origin, keys_1))
-    # {'foo': 'FOO', 'bar': 'BAR', 'foobar': 'fb'}, {'foo': 'F', 'bar': 'BAR', 'foobar': 'fb'}]
-    print("--------------")
     print(filter_values(origin, keys_2))
-    # [{'foo': 'FOO', 'bar': 'BAR', 'foobar': 'fb'}]
-    print("--------------")
-    print(filter_values(origin, keys_3))
-    # [{'foo': 'FOO', 'bar': 'BAR', 'foobar': 'fb'}]
-    print("--------------")
+    print(filter_values(origin, keys_2))

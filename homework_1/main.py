6e515aa328202192a82a838c604385e7d39d0456
Serhii Horodilov <sgorodil@gmail.com>
2022-07-18 14:21:14 +0300

    added tests

--- homework_1/main.py
+++ homework_1/main.py
@@ -1,14 +1,3 @@
-origin = [
-    {"foo": "FOO", "bar": "BAR", "foobar": "fb"},
-    {"foo": "F", "bar": "BAR", "foobar": "fb"},
-    {"foo": "FOO", "bar": "BAR", "foobar": "fb"},
-]
-
-keys_1 = ["foo", "bar"]
-keys_2 = ["foobar"]
-keys_3 = ["bar", "foobar"]
-
-
 def filter_values(data, keys):
     results = []
     substance = []
@@ -22,7 +11,45 @@ def filter_values(data, keys):
     return results
 
 
+fixture = [
+    {"name": "Serhii", "company": "SoftServe", "job": "Software Engineer"},
+    {"name": "Serhii", "company": "Hillel", "job": "Python Trainer"},
+    {"name": "Vlad", "company": "SoftServe", "job": "Release Manager"},
+    {"name": "Vlad", "company": "A-Level", "job": "Python Trainer"},
+    {"name": "Serhii", "company": "A-Level", "job": "Python Trainer"},
+]
+
+
+def test_filter_name():
+    test_value = [
+        {"name": "Serhii", "company": "SoftServe", "job": "Software Engineer"},
+        {"name": "Vlad", "company": "SoftServe", "job": "Release Manager"},
+    ]
+    assert filter_values(fixture, ["name"]) == test_value
+
+
+def test_filter_company():
+    test_value = [
+        {"name": "Serhii", "company": "SoftServe", "job": "Software Engineer"},
+        {"name": "Serhii", "company": "Hillel", "job": "Python Trainer"},
+        {"name": "Vlad", "company": "SoftServe", "job": "Release Manager"},
+        {"name": "Vlad", "company": "A-Level", "job": "Python Trainer"},
+        {"name": "Serhii", "company": "A-Level", "job": "Python Trainer"},
+    ]
+    assert filter_values(fixture, ["name", "company"]) == test_value
+
+
+def test_filter_name_job():
+    test_value = [
+        {"name": "Serhii", "company": "SoftServe", "job": "Software Engineer"},
+        {"name": "Serhii", "company": "Hillel", "job": "Python Trainer"},
+        {"name": "Vlad", "company": "SoftServe", "job": "Release Manager"},
+        {"name": "Vlad", "company": "A-Level", "job": "Python Trainer"},
+    ]
+    assert filter_values(fixture, ["name", "job"]) == test_value
+
+
 if __name__ == "__main__":
-    print(filter_values(origin, keys_1))
-    print(filter_values(origin, keys_2))
-    print(filter_values(origin, keys_2))
+    test_filter_name()
+    test_filter_company()
+    test_filter_name_job()

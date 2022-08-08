b76667e6a173bd1bd470b49eebb45da98334d248
Eugene-TechnoNinja <technoninja@ukr.net>
2022-08-09 01:15:04 +0300

    product cards, models, filter ready

--- meteorites/shop/admin.py
+++ meteorites/shop/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug_name': ['title']
+    }
+    list_display = 'title',
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug_name': ['title']
+    }
+    list_display = 'title', 'location', 'amount', 'category_id', 'category'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- meteorites/shop/views.py
+++ meteorites/shop/views.py
@@ -1,20 +1,41 @@
+from django.http import Http404
 from django.shortcuts import render
-from data import context, userdata
+from shop.models import Product
 
 
 def main(request):
+    context = {
+        'title': 'Main',
+        'products': Product.objects.all()
+    }
+    if request.method == 'POST':
+        country = request.POST.get('country')
+        material_id = request.POST.get('material_id')
+        if country != "0":
+            context['products'] = Product.objects.filter(location=country)
+        if material_id != "0":
+            context['products'] = Product.objects.filter(category_id=material_id)
+        if country != "0" and material_id != "0":
+            context['products'] = Product.objects.filter(location=country, category_id=material_id)
     return render(request, 'shop/main.html', context)
 
 
-def product(request, num):
-    shop_product = {}
-    for value in context['products']:
-        if num == value.get('id'):
-            shop_product = value
-            break
-    shop_product['title'] = 'Buy goods'
-    return render(request, 'product/product.html', shop_product)
+def product(request, slug):
+    try:
+        context = {
+            'title': 'Buy Goods',
+            'product': Product.objects.get(slug_name=slug)
+        }
+        return render(request, 'product/product.html', context)
+    except Product.DoesNotExist:
+        raise Http404('Not found')
 
 
 def profile(request):
-    return render(request, 'user/profile.html', userdata)
+    context = {
+        'title': 'Profile',
+        'firstname': 'Richard',
+        'lastname': 'Stallman',
+        'credit': 10000000,
+    }
+    return render(request, 'user/profile.html', context)
